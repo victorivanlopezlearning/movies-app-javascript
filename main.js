933091d1965576c3751f2475b669c75c35c8e7fd
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-14 22:23:57 -0600

    Added: main archive js

--- main.js
+++ main.js
@@ -1,10 +1,2 @@
-import { MoviesApp } from './src/movies-app';
+import './src/movies-app';
 import './style.css';
-
-document.querySelector('#app').innerHTML = `
-  <div class="content container" id="content"></div>
-`;
-
-const element = document.querySelector('.content');
-
-MoviesApp(element);
