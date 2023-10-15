f2b7e45e5fe2e9bc87c9eec0b9f4be871029f70d
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-14 22:55:53 -0600

    Added: Env Variables

--- src/use-cases/get-movies-by-page.js
+++ src/use-cases/get-movies-by-page.js
@@ -10,7 +10,7 @@ export const GetMoviesByPage = async (page = 1) => {
       method: 'GET',
       headers: {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZTlmNDRjMjJlNmRmZDVlYjQ0MWYwYTQ2MjQ2YWMxMyIsInN1YiI6IjY1MmFmZWUyMGNiMzM1MTZmZDQ5OTlmNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0FEwi3GeapJMwthMNDalye0L_UF6eX9mUclVnV7L5BE'
+        Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`
       }
     };
     const response = await fetch(`${import.meta.env.VITE_API_URL}/3/movie/popular?page=${page}`, options);
